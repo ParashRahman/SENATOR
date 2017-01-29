f6db48d6db586534840376c27d5932afbab28b39
ParashRahman <parash@ualberta.ca>
2017-01-29 03:35:52 -0700

    added sample of what the generated code should look like

--- GenerateGrammar.cpp
+++ GenerateGrammar.cpp
@@ -2,22 +2,54 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
 
 std::map< std::string, std::vector<std::string> >  RULES;
 
 int main() {
   std::string line;
   std::string current_rule;
+
+  std::string title;
+  
+  std::getline(std::cin, title);
   
   while (std::getline(std::cin, line)) {
-    auto colon_loc = line.find(':');
-    if (colon_loc != line.end()) {
-      std::string new_rule = std::string(line.begin(), colon_loc);
+    int colon_index = line.find(':');
+    std::cout << colon_index << std::endl;
 
-      RULES[new_rule] = std::vector<std::string>();
-    }
-
+    auto colon_loc = line.begin() + colon_index;
     
+    if (colon_index == -1) {
+      colon_loc = line.end();
+    }
+        
+    if (colon_loc != line.end()) {
+      current_rule = std::string(line.begin(), colon_loc);
+      RULES[current_rule] = std::vector<std::string>();
+    }
+    
+    RULES[current_rule].push_back(line);
   }
 
+  std::string filePath = "bin/" + title + ".txt";
+  std::ofstream o(filePath);
+  
+  if (o.is_open()) {
+    std::cout << "FILE OPENED! " << filePath << std::endl;
+  } else {
+    std::cout << "FILE UNOPENED :( " << filePath << std::endl;
+  }
+  
+  for (auto map_elem : RULES) {
+    std::string rule_name = map_elem.first;
+    std::vector<std::string> rules = RULES[rule_name];
+
+    for (int i = 0; i < rules.size(); ++i) {
+      // this  is where the rules are processed properly
+      o << rules[i] << std::endl;
+    }
+  }
+  
+  
 }
---
+++ sample.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <cstdlib>
+
+class TITLEGenerator {
+private:
+  std::vector<std::string> Words_txt = {
+    "hi",
+    "my",
+    "name",
+    "is",
+    "parash",
+    "rahman",
+    "and",
+    "im",
+    "here",
+    "to",
+    "say",
+    "all",
+    "you",
+    "people",
+    "are",
+    "super",
+    "cool" };
+public:
+  TITLEGenerator() {
+  }
+  std::string sentence(int complexity) {
+    int NUM_RULES = 1;
+
+    int random_choice = rand() % NUM_RULES;
+    
+    if (complexity <= 0) {
+      std::string p1 = word(complexity - 1);
+      std::string p2 = word(complexity - 1);
+      
+      std::string ret = p1 + " " + p2;
+      return ret;
+    }
+    else if (random_choice == 0) {
+      std::string p1 = word(complexity - 1);
+      std::string p2 = word(complexity - 1);
+      
+      std::string ret = p1 + " " + p2;
+      return ret;
+    }
+    
+    printf("ERRAR SENTENCE\n");
+    exit(1);
+  }
+
+  std::string word(int complexity) {
+    int NUM_RULES = 2;
+    
+    int random_choice = rand() % NUM_RULES;
+    
+    if (complexity <= 0) {
+      // base case goes here
+      
+      int dex = rand() % Words_txt.size();
+
+      std::string ret = Words_txt[dex];
+      return ret;
+    }
+    else if (random_choice == 0) {
+      int dex = rand() % Words_txt.size();
+
+      std::string ret = Words_txt[dex];
+      return ret;
+    }
+    else if (random_choice == 1) {
+      std::string p1 = word(complexity - 1);
+      std::string p2 = word(complexity - 1);
+
+      std::string ret = p1 + " " + p2;
+      return ret;
+    }
+
+    printf("ERRAR WORD\n");
+    exit(1);
+  }
+};
+/*
+int main() {
+  TITLEGenerator tG;
+  
+  std::cout << tG.sentence(3) << std::endl;
+}
+*/
